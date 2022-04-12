fd441108ceb43956ece28c80d8eb852fbab96ea9
misiucodes <meeshtran15@gmail.com>
2022-04-11 22:15:16 -0400

    added middle section

--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,6 @@
 import React from "react";
 import egg from "./images/egg.jpg";
+import standout from "./images/standout.jpg"
 import "./Body.css";
 
 function Body({
@@ -14,20 +15,41 @@ function Body({
 
       <div className="row main_row">
 
-          <div className="col text-wrapper">
+          <div className="col text-wrapper-top">
             <div className="top-line">
               Transform your brand
             </div>
             <p className="description">
               We are a full-service creative agency specializing in helping brands grow fast. Engage your clients through compelling visuals that do most of the marketing for you.
             </p>
-            <button className="btn-learnmore">
+            <button className="btn-transform">
               LEARN MORE
             </button>
         </div>
 
         <div className="col img-wrapper">
-          <img src={egg} alt="egg" className="img-main-pg"></img>
+          <img src={egg} alt="egg" className="img-main-pg" width="950"></img>
+        </div>
+
+        
+
+      </div>
+
+      <div className="row middle_row">
+
+        <div className="col img-wrapper">
+          <img src={standout} alt="pink cup" className="img-main-pg" ></img>
+        </div>
+        <div className="col text-wrapper-top">
+          <div className="top-line">
+            Stand out to the right audience
+          </div>
+          <p className="description">
+            Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we'll build and extend your brand in digital places.
+          </p>
+          <button className="btn-standout">
+            LEARN MORE
+          </button>
         </div>
 
       </div>
